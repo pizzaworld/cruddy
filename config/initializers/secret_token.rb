c6dbfecfbe29c4c4f20eb32f6159c80929fdab6a
Sara Cavic <saracavic@gmail.com>
2014-09-01 19:23:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cruddy::Application.config.secret_key_base = '6395c96aec561e3840aab2378e6f763b9e71c222c1c780838ee1261d767791dfa07c4fa7256ed65002cda20965300210ec0622869f22ab71c17ed5b87dc53f18'
