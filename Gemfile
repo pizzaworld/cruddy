590ef142cf5ff4691e994fbe3f56f4d4fd648931
Sara Cavic <saracavic@gmail.com>
2014-09-13 22:21:01 -0400

    Bundle factory_girl_rails

--- Gemfile
+++ Gemfile
@@ -16,5 +16,6 @@ end
 
 group :test do
   gem 'capybara'
+  gem 'factory_girl_rails'
   gem 'rspec-rails'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -19,6 +20,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
   config.include Capybara::DSL, :type => :feature
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
